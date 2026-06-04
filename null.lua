5419f6eb95f4ea26b88bde5dff5fdf411ecc6320
Conduit <153644058+MangoPlayzXD@users.noreply.github.com>
2026-06-03 23:34:42 -0700

    Change addUpgrade parameter to a string

--- null.lua
+++ null.lua
@@ -3727,7 +3727,7 @@ UserInputService.InputBegan:Connect(function(input, gameProcessed)
 		notif("Detected", "Cool")
 		local v = 30
 		magnet:Fire({Add = v})
-		addUpgrade("NinjaBelt", 5)
+		addUpgrade("NinjaBelt", "Current:0")
 		-- Put your code here
 		-- Example:
 		-- game.ReplicatedStorage.MyRemoteEvent:FireServer()
